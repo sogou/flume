9ceff008df877de691f67b8b9f76aac98052e0cc
Tao Li <litao@sogou-inc.com>
2015-05-01 00:39:55 +0800

    Add custom TimedSinkCounter to aggregate countes with five min.

---
+++ flume-ng-core/src/main/java/org/apache/flume/instrumentation/sogou/TimedSinkCounter.java
@@ -0,0 +1,35 @@
+package org.apache.flume.instrumentation.sogou;
+
+import org.apache.flume.Event;
+import org.apache.flume.instrumentation.SinkCounter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Tao Li on 4/29/15.
+ */
+public class TimedSinkCounter extends SinkCounter implements TimedSinkCounterMBean {
+  private Map<String, Map<String, TimedUtils.TimestampCount>> eventDrainSuccessCountInFiveMinMap =
+          new HashMap<String, Map<String, TimedUtils.TimestampCount>>();
+
+  private static final String COUNTER_EVENT_DRAIN_SUCCESS_IN_FIVE_MIN =
+          "sink.event.drain.sucess.5min";
+
+  private static final String[] ATTRIBUTES =
+          {COUNTER_EVENT_DRAIN_SUCCESS_IN_FIVE_MIN};
+
+  public TimedSinkCounter(String name) {
+    super(name, ATTRIBUTES);
+  }
+
+  public void addToEventDrainSuccessCountInFiveMinMap(List<Event> events) {
+    TimedUtils.updateFiveMinMap(events, eventDrainSuccessCountInFiveMinMap);
+  }
+
+  @Override
+  public String getEventDrainSuccessCountInFiveMinJson() {
+    return TimedUtils.convertFiveMinMapToJson(eventDrainSuccessCountInFiveMinMap);
+  }
+}
---
+++ flume-ng-core/src/main/java/org/apache/flume/instrumentation/sogou/TimedSinkCounterMBean.java
@@ -0,0 +1,10 @@
+package org.apache.flume.instrumentation.sogou;
+
+import org.apache.flume.instrumentation.SinkCounterMBean;
+
+/**
+ * Created by Tao Li on 4/29/15.
+ */
+public interface TimedSinkCounterMBean extends SinkCounterMBean {
+  String getEventDrainSuccessCountInFiveMinJson();
+}
