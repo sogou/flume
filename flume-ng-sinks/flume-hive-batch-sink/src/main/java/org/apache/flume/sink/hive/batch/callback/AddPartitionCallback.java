ad9f0746c3e255ab5fa1fff6f44919e495160bd2
Tao Li <litao@sogou-inc.com>
2016-02-26 19:41:49 +0800

    Update AddPartitionCallback, catch AlreadyExistsException.

--- flume-ng-sinks/flume-hive-batch-sink/src/main/java/org/apache/flume/sink/hive/batch/callback/AddPartitionCallback.java
+++ flume-ng-sinks/flume-hive-batch-sink/src/main/java/org/apache/flume/sink/hive/batch/callback/AddPartitionCallback.java
@@ -2,6 +2,7 @@ package org.apache.flume.sink.hive.batch.callback;
 
 import org.apache.flume.sink.hive.batch.HiveBatchWriter;
 import org.apache.flume.sink.hive.batch.util.HiveUtils;
+import org.apache.hadoop.hive.metastore.api.AlreadyExistsException;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,8 +32,10 @@ public class AddPartitionCallback implements HiveBatchWriter.Callback {
   public void run() {
     try {
       HiveUtils.addPartition(dbName, tableName, values, location);
+    } catch (AlreadyExistsException e) {
+      LOG.warn("Partition already exists: " + dbName + "." + tableName + " " + values);
     } catch (TException e) {
-      LOG.error("fail to add partition: " + dbName + ", " + tableName + ", " + values, e);
+      LOG.error("Fail to add partition: " + dbName + "." + tableName + " " + values, e);
     }
   }
 }
